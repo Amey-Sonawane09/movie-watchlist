e4e8ff2ef7d3d4b6fe18249f1ca0c00ca1d90bf4
Amey Sonawane <ameysonawane259@gmail.com>
2025-07-03 03:05:03 +0530

    Update server.js for non-localhost requests

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(8000, '0.0.0.0', () => {
+    console.log("Server running on port 8000");
 });
